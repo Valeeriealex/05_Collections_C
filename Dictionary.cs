9f530ae388548d905becfb09a98d79345938992a
Valerie <114693445+Valeeriealex@users.noreply.github.com>
2024-10-17 23:11:43 +0300

    Update Dictionary.cs
    Создать программу, которая принимает от пользователя слово и выводит его значение.
    Если такого слова нет, то следует вывести соответствующее сообщение.

--- Dictionary.cs
+++ Dictionary.cs
@@ -1,65 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-//namespace Tasks_IJunior._05_Collections
-//{
-//    internal class Dictionary
-//    {
-//        //Создать программу, которая принимает от пользователя слово и выводит его значение.
-//        //Если такого слова нет, то следует вывести соответствующее сообщение.
+namespace Tasks_IJunior._05_Collections
+{
+   internal class Dictionary
+   {
 
-//        static void Main(string[] args)
-//        {
-//            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+       static void Main(string[] args)
+       {
+           Dictionary<string, string> dictionary = new Dictionary<string, string>();
 
-//            dictionary.Add("переменная", "это поименованная, либо адресуемая иным способом область памяти, адрес которой можно использовать для осуществления доступа к данным и изменять значение в ходе выполнения программы.");
-//            dictionary.Add("условные операторы", "созданы для управления процессом выполнения программы условные операторы: if и switch, а для запуска повторяющихся действий циклы: for, while (do/while) и foreach.");
-//            dictionary.Add("массив", "это упорядоченный набор элементов, каждый из которых хранит одно значение, идентифицируемое с помощью одного или нескольких индексов, бывает одномерный и многомерный.");
-//            dictionary.Add("функции", "это фрагмент программного кода, к которому можно обратиться из другого места программы.");
-//            dictionary.Add("коллекции", "это гибкий способ работы с группами объектов: List, Queue, Stack, Dictionary.");
+           dictionary.Add("переменная", "это поименованная, либо адресуемая иным способом область памяти, адрес которой можно использовать для осуществления доступа к данным и изменять значение в ходе выполнения программы.");
+           dictionary.Add("условные операторы", "созданы для управления процессом выполнения программы условные операторы: if и switch, а для запуска повторяющихся действий циклы: for, while (do/while) и foreach.");
+           dictionary.Add("массив", "это упорядоченный набор элементов, каждый из которых хранит одно значение, идентифицируемое с помощью одного или нескольких индексов, бывает одномерный и многомерный.");
+           dictionary.Add("функции", "это фрагмент программного кода, к которому можно обратиться из другого места программы.");
+           dictionary.Add("коллекции", "это гибкий способ работы с группами объектов: List, Queue, Stack, Dictionary.");
 
-//            bool isWorking = true;
+           bool isWorking = true;
 
-//            while (isWorking)
-//            {
-//                string entrance = "1";
-//                string exit = "2";
+           while (isWorking)
+           {
+               string entrance = "1";
+               string exit = "2";
 
-//                Console.WriteLine($"\nСловарь.\n{entrance}- начать поиск знаения слов\n{exit} - выйти\n");
-//                string userChoice = Console.ReadLine();
+               Console.WriteLine($"\nСловарь.\n{entrance}- начать поиск знаения слов\n{exit} - выйти\n");
+               string userChoice = Console.ReadLine();
 
-//                if (userChoice == entrance)
-//                {
-//                    ScreachWorld(dictionary);
-//                }
-//                else if (userChoice == exit)
-//                {
-//                    isWorking = false;
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Неверный выбор. Попробуйте еще раз.");
-//                }
-//            }
-//        }
+               if (userChoice == entrance)
+               {
+                   ScreachWorld(dictionary);
+               }
+               else if (userChoice == exit)
+               {
+                   isWorking = false;
+               }
+               else
+               {
+                   Console.WriteLine("Неверный выбор. Попробуйте еще раз.");
+               }
+           }
+       }
 
-//        static void ScreachWorld(Dictionary<string, string> dictionary)
-//        {
-//            Console.WriteLine("\nВведите слово: ");
-//            string userWorld = Console.ReadLine();
-//            string author = userWorld.ToLower();
+       static void ScreachWorld(Dictionary<string, string> dictionary)
+       {
+           Console.WriteLine("\nВведите слово: ");
+           string userWorld = Console.ReadLine();
+           string author = userWorld.ToLower();
 
-//            if (dictionary.ContainsKey(author))
-//            {
-//                Console.WriteLine(dictionary[author]);
-//            }
-//            else
-//            {
-//                Console.WriteLine("Такого слова нет");
-//            }
-//        }
-//    }
-//}
+           if (dictionary.ContainsKey(author))
+           {
+               Console.WriteLine(dictionary[author]);
+           }
+           else
+           {
+               Console.WriteLine("Такого слова нет");
+           }
+       }
+   }
+}
